be8806caf53d7546fd0d94c367ebdd260eef75a5
Jonas Bernoulli <jonas@bernoul.li>
2022-09-15 22:30:27 +0200

    Update headers to follow more conventions

--- pg.el
+++ pg.el
@@ -1,29 +1,26 @@
 ;;; pg.el --- Emacs Lisp interface to the PostgreSQL RDBMS
-;;;
-;;; Author: Eric Marsden <emarsden@laas.fr>
-;;; Maintainer: Helmut Eller <heller@common-lisp.net>
-;;; Version: 0.13+ (sorta)
-;;; Keywords: data comm database postgresql
-;;;
-;;; Copyright: (C) 1999-2005  Eric Marsden
-;;; Copyright: (C) 2005-2006  Eric Marsden, Helmut Eller
 ;;
-;;     This program is free software; you can redistribute it and/or
-;;     modify it under the terms of the GNU General Public License as
-;;     published by the Free Software Foundation; either version 2 of
-;;     the License, or (at your option) any later version.
-;;
-;;     This program is distributed in the hope that it will be useful,
-;;     but WITHOUT ANY WARRANTY; without even the implied warranty of
-;;     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-;;     GNU General Public License for more details.
-;;
-;;     You should have received a copy of the GNU General Public
-;;     License along with this program; if not, write to the Free
-;;     Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
-;;     MA 02111-1307, USA.
-;;
+;; Copyright (C) 1999-2006  Eric Marsden
+;; Copyright (C) 2005-2006  Helmut Eller
 
+;; Author: Eric Marsden <emarsden@laas.fr>
+;; Maintainer: Helmut Eller <heller@common-lisp.net>
+;; Version: 0.13+ (sorta)
+;; Keywords: data comm database postgresql
+;; SPDX-License-Identifier: GPL-2.0-or-later
+;;
+;; This file is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published
+;; by the Free Software Foundation, either version 2 of the License,
+;; or (at your option) any later version.
+;;
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this file.  If not, see <https://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 
